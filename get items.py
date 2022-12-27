b23e0cdbb6b53d87fac8fa4c36281b38d5d3d1af
Brenda-Mwangi <sheecnc@gmail.com>
2022-12-27 20:04:53 +0300

    added number guessing game

---
+++ get items.py
@@ -0,0 +1,14 @@
+data = [
+    {'name': 'Joe', 'age': 20},
+    {'name': 'Bill', 'age': 30},
+    {'name': 'Kate', 'age': 23}
+]
+# def itemgetter(item):
+#     item['name']
+    
+def get_names(data):
+    for i in data:
+        x = print(i)
+    return x
+
+get_names(data)    
