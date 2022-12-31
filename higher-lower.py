da2458c2ceb1af0d31125c1b8ea48d8d86f51209
Brenda-Mwangi <sheecnc@gmail.com>
2022-12-31 08:22:47 +0300

    final correct logic

--- higher-lower.py
+++ higher-lower.py
@@ -1,32 +1,30 @@
 from art import logo, vs
 from game_data import data
 import random as r
-# print(logo)
+print(logo)
 
 
-A = data[r.randint(0, 49)]
-B = data[r.randint(0,49)]
-# ANSWER = input("Who has more followers? Type 'A' or 'B': ")
+
 CORRECT = 1
 WRONG = 0
-# print(A)
-# print(B)
-def increase():
-    i = 0
-    
+
+
 
 def to_compare():
-    print(f"Compare A: {A.get('name')}, a {A.get('description')}, from {A.get('country')}.")
+    a = data[r.randint(0, 49)]
+    b = data[r.randint(0,49)]
+
+    print(f"Compare A: {a.get('name')}, a {a.get('description')}, from {a.get('country')}.")
     print(vs)
-    print(f"Against B: {B.get('name')}, a {B.get('description')}, from {B.get('country')}.")
-    # print(A.get("follower_count"))
-    # print(B.get("follower_count"))
+    print(f"Against B: {b.get('name')}, a {b.get('description')}, from {b.get('country')}.")
+
     ANSWER = input("Who has more followers? Type 'A' or 'B': ")
 
-
-# def compare_answer():
     if ANSWER == 'A' or ANSWER == 'a':
-        if A.get("follower_count") > B.get("follower_count"):
+        if a.get("follower_count") > b.get("follower_count"):
+            print("Correct!")
+            return CORRECT
+        elif a.get("follower_count") == b.get("follower_count"):
             print("Correct!")
             return CORRECT
         else:
@@ -34,11 +32,30 @@ def to_compare():
             return WRONG
 
     elif ANSWER == 'B' or ANSWER == 'b' :
-        if B.get("follower_count")> A.get("follower_count"):
+        if b.get("follower_count")> a.get("follower_count"):
+            print("Correct!")
+            return CORRECT
+        elif b.get("follower_count") == a.get("follower_count"):
+            print("Correct!")
             return CORRECT
         else:
+            print("Wrong!")
             return WRONG
     else:
         print("Not valid.")
+        return WRONG
 
-to_compare()
+def increase():
+    score = 0
+    if 1 == 1:
+        while score >= 0:
+            print(f"Your score: {score}\n")
+            x = to_compare()  #1 or 0
+            if x == 0:
+                print(f"Game Over!! Your final score is {score}")
+                break
+            else:
+                score+=x
+                continue
+          
+increase()
