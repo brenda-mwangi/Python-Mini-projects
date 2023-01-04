a34dbe207eaeab890f606ba438f5997accffff8b
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-04 23:54:20 +0300

    added new data from https://opentdb.com/

--- 100 days of code projects/Day 17-start quiz game/data.py
+++ 100 days of code projects/Day 17-start quiz game/data.py
@@ -1,14 +1,13 @@
-question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+question_data =[
+                    
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "hard", "question": "This is the correct spelling of Supercalifragilisticexpialidocious.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The bikini is named after the Bikini Atoll, an island where the United States conducted tests on atomic bombs.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "The color orange is named after the fruit.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "When you cry in space, your tears stick to your face.", "correct_answer": "True", "incorrect_answers": ["False"]},
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "Scotland voted to become an independent country during the referendum from September 2014.", "correct_answer": "False", "incorrect_answers": ["True"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The scientific name for the Southern Lights is Aurora Australis?", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The British organisation CAMRA stands for The Campaign for Real Ale.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+                    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "Jingle Bells was originally meant for Thanksgiving", "correct_answer": "True", "incorrect_answers": ["False"]},
+                    ]
