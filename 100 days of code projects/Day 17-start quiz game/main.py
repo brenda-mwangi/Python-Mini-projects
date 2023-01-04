61be53d15cefa0f7b7c1862260d6a89f7e9246e7
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-04 23:54:27 +0300

    Update main.py

--- 100 days of code projects/Day 17-start quiz game/main.py
+++ 100 days of code projects/Day 17-start quiz game/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in data:
-    question_text=question['text']
-    question_answer=question['answer']
+    question_text=question['question']
+    question_answer=question['correct_answer']
     new_quest = Question(question_text,question_answer)
     question_bank.append(new_quest)
 
