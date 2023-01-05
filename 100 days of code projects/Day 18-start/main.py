1cb96c82c3d2b06b26c60eca20635e22bc668059
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-05 11:47:11 +0300

    Learning the turtle module.
    In this chapter, I am using the turtle module to create graphics. It is also a good way to learn how to use documentation.

---
+++ 100 days of code projects/Day 18-start/main.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+timmy = Turtle()
+
+timmy.shape("circle")
+timmy.color("pink")
+
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+screen= Screen()
+screen.exitonclick()
