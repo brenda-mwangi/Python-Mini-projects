a8bdfea9efd0abdbc6be27579f3c9ef5c6f3b7c5
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-09 22:39:40 +0300

    Update learn-main.py

--- 100 days of code projects/Day 27 Tkinter Intro/learn-main.py
+++ 100 days of code projects/Day 27 Tkinter Intro/learn-main.py
@@ -1,17 +1,38 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20) #padding all round the screen
 
+def got_click():
+    label1.config(text=input.get())
+    print("Button Got Clicked")
 # label
-label = tkinter.Label(text="Me Label", font=("Arial", 24))
-label.pack(expand=True)
 
-label1 = tkinter.Label(text="Me Label2", font=("Arial", 24, "italic"))
-label1.pack(side="left")
+# label["text"] = "My label"
+# label.config(text="New text2")
+label1 = Label(text="Me Label2", font=("Arial", 24, "italic"))
+# label1.place(x=100,y=50)
+label1.grid(column=0,row=0)
+label1.config(padx=20,pady=20)
+# label2 = tkinter.Label(text="Me Label3", font=("Arial", 24, "bold"))
+# label2.pack()
 
-label2 = tkinter.Label(text="Me Label3", font=("Arial", 24, "bold"))
-label2.pack()
+# Button
+def got_click():
+    label1.config(text=input.get())
+    # print("Button Got Clicked")
+
+button = Button(text="Click Me", command=got_click)
+# button.pack()
+button.grid(column=1,row=1)
+
+button2 = Button(text="New button")
+button2.grid(column=2, row=0)
+# Entry or input
+input = Entry(width=30)
+input.grid(column=3,row=2)
+
 
 window.mainloop()
