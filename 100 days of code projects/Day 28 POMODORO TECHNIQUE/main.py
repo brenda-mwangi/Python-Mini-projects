84e8cd14690d5d78a8524e310d5194a3d1b21a21
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-11 19:51:57 +0300

    Update main.py

--- 100 days of code projects/Day 28 POMODORO TECHNIQUE/main.py
+++ 100 days of code projects/Day 28 POMODORO TECHNIQUE/main.py
@@ -28,18 +28,13 @@ def start_timer():
         countdown(int(LONG_BREAK_MIN))
         timer.config(text="BREAK",fg=RED)
 
-
     elif reps %2 == 0:
         countdown(int(SHORT_BREAK_MIN))
         timer.config(text="BREAK",fg=PINK)
 
-
     else:
         countdown(int(WORK_MIN))
         timer.config(text="WORK",fg=GREEN)
-
-        
-
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 from math import floor
@@ -66,7 +61,6 @@ def countdown(count):
             marks += "✔"
             tick.config(text=marks)
         
-
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -80,14 +74,18 @@ canvas.create_image(100,112, image = tomato)
 timer_text=canvas.create_text(100,130, text="00:00", fill= "white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(row=1,column=1)
 
+
 timer = Label(text="Timer", font=("Courier", 50), fg=GREEN, bg=YELLOW, padx=10, pady=10)
 timer.grid(row=0,column=1)
+
 
 start = Button(text = "Start",command=start_timer)
 start.grid(row=2,column=0)
 
+
 resetter = Button(text="Reset", command=reset)
 resetter.grid(row=2,column=2)
+
 
 tick = Label(font=("Courier"), fg=GREEN, bg=YELLOW)
 tick.grid(row=3,column=1)
