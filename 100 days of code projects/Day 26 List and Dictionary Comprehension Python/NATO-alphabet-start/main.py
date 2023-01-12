8cd3565fa8f85d15b599047342b29811013123aa
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-12 20:47:43 +0300

    Update after catching errors and handling exceptions

--- 100 days of code projects/Day 26 List and Dictionary Comprehension Python/NATO-alphabet-start/main.py
+++ 100 days of code projects/Day 26 List and Dictionary Comprehension Python/NATO-alphabet-start/main.py
@@ -1,10 +1,21 @@
+# {new_key:new_value for (index, row) in df.iterrows()}
 import pandas as pd
+
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
-alphabet_df = pd.read_csv("./NATO-alphabet-start/nato_phonetic_alphabet.csv")
+
+alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name: ").upper()
-nato = [alphabet_dict[n] for n in user_input]
-print(nato)
+def nato():
+    user_input = input("Enter your a word: ").upper()
+    try:
+        output_list = [alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato()
+    else:
+        print(output_list)
+nato()
+
