91374d56bbc3f1691971c26ebbcf57dc90142495
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-16 09:51:07 +0300

    Update main.py

--- 100 days of code projects/Day 32 Birthday Wisher/main.py
+++ 100 days of code projects/Day 32 Birthday Wisher/main.py
@@ -2,11 +2,11 @@ import smtplib
 
 my_email = "sheecnc@gmail.com"
 msg = "\n\n".join([
-  "Subject: Just a message",
-  "Why, oh why"
+  "Subject: Dear Brian",
+  "How are you?"
   ])
 with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.ehlo()
     connection.starttls()
     connection.login(user=my_email, password="wznmzkpaotyyrucu")
-    connection.sendmail(from_addr=my_email, to_addrs="breshshilaxy@yahoo.com", msg=msg)
+    connection.sendmail(from_addr=my_email, to_addrs="bryankamaa09@gmail.com", msg=msg)
