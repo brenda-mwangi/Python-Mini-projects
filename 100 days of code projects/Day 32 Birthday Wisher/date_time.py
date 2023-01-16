01a883db3d0bb35db21f17bd1a6691c20beaed9f
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-16 21:01:50 +0300

    The date time module

---
+++ 100 days of code projects/Day 32 Birthday Wisher/date_time.py
@@ -0,0 +1,16 @@
+import datetime as dt
+
+now = dt.datetime.now()
+
+now.minute
+now.hour
+now.second
+now.month
+now.year
+now.day
+now.weekday()
+
+
+date_of_birth = dt.datetime(year=2020,month=1,day=16)
+
+print(date_of_birth)
