f4c0b3361099048b0da957a8d356c681ad112c6a
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-18 01:45:42 +0300

    Working with Responses_ HTTP Codes, Exceptions & JSON Data

--- 100 days of code projects/Day 33 APIs/main.py
+++ 100 days of code projects/Day 33 APIs/main.py
@@ -1,1 +1,9 @@
 #working with APIs
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # returns a response code <Response [200]>
+# print(response)
+
+# returns a response code "200"
+print(response.status_code)
