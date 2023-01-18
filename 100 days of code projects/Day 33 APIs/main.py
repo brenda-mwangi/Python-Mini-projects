f35e73add2411b90ee229561a24849c0a0bf3257
Brenda-Mwangi <sheecnc@gmail.com>
2023-01-18 22:10:51 +0300

    reading API data using json files

--- 100 days of code projects/Day 33 APIs/main.py
+++ 100 days of code projects/Day 33 APIs/main.py
@@ -8,5 +8,18 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # returns a response code "200"
 # print(response.status_code)
 
-if response.status_code !=200:
-    raise Exception("Bad =")
+# # raise error for the status code
+# if response.status_code == 400:
+#     raise Exception("Does not exist")
+# elif response.status_code == 401:
+#     raise Exception("Not authorized.")
+
+# response.raise_for_status()
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(data)
+print(iss_position)
