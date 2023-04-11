a1f8c382e0485e284fbf2860a9bed9f345498ca0
Brenda-Mwangi01 <sheecnc@gmail.com>
2023-04-11 10:54:36 +0300

    Update newmessage.py

--- newmessage.py
+++ newmessage.py
@@ -3,9 +3,8 @@ import pywhatkit as kit
 with open('names.txt', 'r') as file:
     numbers = [i.strip() for i in file.readlines()]
     print(numbers)
-
 for n in numbers:
     print(n)
 # Send a message to a contact on WhatsApp
-    kit.sendwhatmsg_instantly(f"{n}", "Good evening!.\nI hope you are doing well. This message is to remind you that we are each contributing 500/= for the *Gratitude Geeks Party*. I have not received your contribution and the deadline, 21st March, is fast approaching. Please send your contribution on time for proper logistics and planning.\n*Brenda Mwangi*,\nClub Treasurer.", 10, True,3)
+    kit.sendwhatmsg_instantly(f"{n}", "Good morning!.\n I have not received your 500/=  contribution for the *Gratitude Geeks Party* contribution. Today is the only day left. Please send your contribution on time for proper logistics and planning. \nM-pesa number: 0740730056\nM-pesa name: Charles Gitau\n\nYours, *Brenda Mwangi*,\nClub Treasurer.", 45, True,15)
 
