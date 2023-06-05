bdab597bab901d05baba91f247262564855defd9
Brenda-Mwangi01 <sheecnc@gmail.com>
2023-06-05 16:30:51 +0300

    Self created functions

--- 100 days of code projects/DAY 50 - FLASK  FRAMEWORK/Hello.py
+++ 100 days of code projects/DAY 50 - FLASK  FRAMEWORK/Hello.py
@@ -2,13 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}<b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}<em>"
+    return wrapper
+
+def make_underline(function):
+    def wrapper():
+        return f"<u>{function()}<u>"
+    return wrapper
+
 @app.route("/")
 def hello_world():
     return "<p style='color: blue;'>Hello, World!</p>"
 
+
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
 def say_bye():
-    return "bye"
+    return "Bye"
 
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
