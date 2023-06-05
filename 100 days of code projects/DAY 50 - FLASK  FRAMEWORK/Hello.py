ed7f3295478c7fab225906bcb4bb1d8bb6cf68ed
Brenda-Mwangi01 <sheecnc@gmail.com>
2023-06-05 16:00:28 +0300

    routing using flask app decorator, routing using variables

--- 100 days of code projects/DAY 50 - FLASK  FRAMEWORK/Hello.py
+++ 100 days of code projects/DAY 50 - FLASK  FRAMEWORK/Hello.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return "<p style='color: blue;'>Hello, World!</p>"
 
+@app.route("/bye")
+def say_bye():
+    return "bye"
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name}, you're {number} years!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
